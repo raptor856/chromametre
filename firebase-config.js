41089c206e6376de3a41f569fee3becd5cfee5db
raptor856 <anthonygoatdiscord@gmail.com>
2026-07-02 02:02:34 +0200

    ajout html et js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Config Firebase du projet "jeu-des-couleurs"
+export const firebaseConfig = {
+  apiKey: "AIzaSyBGktjkylFJ5hKGh8RQ9hx_auH69boe09g",
+  authDomain: "jeu-des-couleurs.firebaseapp.com",
+  projectId: "jeu-des-couleurs",
+  storageBucket: "jeu-des-couleurs.firebasestorage.app",
+  messagingSenderId: "198620034616",
+  appId: "1:198620034616:web:140a70106b8ed69be698cd",
+  measurementId: "G-JLKXBWLZDS"
+};
